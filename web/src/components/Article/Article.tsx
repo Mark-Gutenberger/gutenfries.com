0a7b3a81b5102bec3cf5b931d2aa8a9f8085947e
mark gutenberger <mark-gutenberger@outlook.com>
2022-06-29 21:54:31 -0500

    add commenting system

--- web/src/components/Article/Article.tsx
+++ web/src/components/Article/Article.tsx
@@ -1,21 +1,28 @@
 import { Link, routes } from '@redwoodjs/router';
+import CommentsCell from 'src/components/CommentsCell';
+import CommentForm from 'src/components/CommentForm';
 
-import type { Post } from 'types/graphql';
+const truncate = (text: string, length: number) => {
+	return text.substring(0, length) + '...';
+};
 
-interface Props {
-	article: Post;
-}
-
-const Article = ({ article }: Props) => {
+const Article = ({ article, summary = false }) => {
 	return (
 		<article>
 			<header>
-				<h2>
+				<h2 className='text-xl text-blue-700 font-semibold'>
 					<Link to={routes.article({ id: article.id })}>{article.title}</Link>
 				</h2>
 			</header>
-			<div>{article.body}</div>
-			<div>Posted at: {article.createdAt}</div>
+			<div className='mt-2 text-gray-900 font-light'>{summary ? truncate(article.body, 100) : article.body}</div>
+			{!summary && (
+				<div className='mt-12'>
+					<CommentForm postId={article.id} />
+					<div className='mt-12'>
+						<CommentsCell postId={article.id} />
+					</div>
+				</div>
+			)}
 		</article>
 	);
 };
--- web/src/components/ArticlesCell/ArticlesCell.stories.tsx
+++ web/src/components/ArticlesCell/ArticlesCell.stories.tsx
@@ -1,20 +1,8 @@
-import { Loading, Empty, Failure, Success } from './ArticlesCell';
+import { Success } from './ArticlesCell';
 import { standard } from './ArticlesCell.mock';
 
-export const loading = () => {
-	return Loading ? <Loading /> : null;
-};
-
-export const empty = () => {
-	return Empty ? <Empty /> : null;
-};
-
-export const failure = () => {
-	return Failure ? <Failure error={new Error('Oh no')} /> : null;
-};
-
 export const success = () => {
-	return Success ? <Success {...standard()} /> : null;
+	return Success ? <Success articles={standard().articles} /> : null;
 };
 
 export default { title: 'Cells/ArticlesCell' };
