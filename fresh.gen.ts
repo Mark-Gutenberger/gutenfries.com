d97ee57056d806850fba6d7732dc291e92263d7c
mark gutenbegrer <mark-gutenberger@outlook.com>
2022-09-14 08:02:06 -0500

    update manifest

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -11,13 +11,12 @@ import * as $4 from './routes/api/tests/version.test.ts';
 import * as $5 from './routes/api/v1/github/[username].tsx';
 import * as $6 from './routes/api/v1/greet/[name].tsx';
 import * as $7 from './routes/api/v1/joke.ts';
-import * as $8 from './routes/api/v1/markdown/[filepath].tsx';
-import * as $9 from './routes/api/v1/random-uuid.ts';
-import * as $10 from './routes/api/v1/theme-generator.ts';
-import * as $11 from './routes/api/v1/version.ts';
-import * as $12 from './routes/api/v1/wasm.ts';
-import * as $13 from './routes/home.tsx';
-import * as $14 from './routes/index.tsx';
+import * as $8 from './routes/api/v1/random-uuid.ts';
+import * as $9 from './routes/api/v1/theme-generator.ts';
+import * as $10 from './routes/api/v1/version.ts';
+import * as $11 from './routes/api/v1/wasm.ts';
+import * as $12 from './routes/home.tsx';
+import * as $13 from './routes/index.tsx';
 
 const manifest = {
 	routes: {
@@ -29,13 +28,12 @@ const manifest = {
 		'./routes/api/v1/github/[username].tsx': $5,
 		'./routes/api/v1/greet/[name].tsx': $6,
 		'./routes/api/v1/joke.ts': $7,
-		'./routes/api/v1/markdown/[filepath].tsx': $8,
-		'./routes/api/v1/random-uuid.ts': $9,
-		'./routes/api/v1/theme-generator.ts': $10,
-		'./routes/api/v1/version.ts': $11,
-		'./routes/api/v1/wasm.ts': $12,
-		'./routes/home.tsx': $13,
-		'./routes/index.tsx': $14,
+		'./routes/api/v1/random-uuid.ts': $8,
+		'./routes/api/v1/theme-generator.ts': $9,
+		'./routes/api/v1/version.ts': $10,
+		'./routes/api/v1/wasm.ts': $11,
+		'./routes/home.tsx': $12,
+		'./routes/index.tsx': $13,
 	},
 	islands: {},
 	baseUrl: import.meta.url,
