0a10c1de9718d1285c94ed2923abcfe63b36c364
mark gutenberger <mark-gutenberger@outlook.com>
2022-09-16 13:34:23 -0500

    remove Fragment from about route

--- routes/about.tsx
+++ routes/about.tsx
@@ -3,19 +3,17 @@ import { PageProps } from '$fresh/server.ts';
 
 function AboutPage(pageProps_: PageProps) {
 	return (
-		<>
-			<MainLayout
-				pageProps_={pageProps_}
-			>
-				<br></br>
-				<p>
-					How come you don't fly everywhere? It's exhausting. Why don't you run
-					everywhere? It's faster. Yeah, OK, I see, I see. All right, your turn. TiVo. You
-					can just freeze live TV? That's insane! You don't have that? We have Hivo, but
-					it's a disease. It's a horrible, horrible disease.
-				</p>
-			</MainLayout>
-		</>
+		<MainLayout
+			pageProps_={pageProps_}
+		>
+			<br></br>
+			<p>
+				How come you don't fly everywhere? It's exhausting. Why don't you run everywhere?
+				It's faster. Yeah, OK, I see, I see. All right, your turn. TiVo. You can just freeze
+				live TV? That's insane! You don't have that? We have Hivo, but it's a disease. It's
+				a horrible, horrible disease.
+			</p>
+		</MainLayout>
 	);
 }
 
