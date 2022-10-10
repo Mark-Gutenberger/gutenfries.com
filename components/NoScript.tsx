8a6b305aea68a97907a1786e824d563f0ad2f536
Mark Gutenberger <79616833+gutenfries@users.noreply.github.com>
2022-10-10 09:37:08 -0500

    (Bug): change h-screen to h-full

--- components/NoScript.tsx
+++ components/NoScript.tsx
@@ -4,7 +4,7 @@ import { h } from 'preact';
 function NoScript() {
 	return (
 		<noscript>
-			<div className='flex flex-col items-center justify-center h-screen'>
+			<div className='flex flex-col items-center justify-center h-full'>
 				<h2 className='text-4xl font-bold text-center text-gray-200'>
 					Please enable JavaScript for the best experience using this website.
 				</h2>
