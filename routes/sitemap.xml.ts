e5aa4458be86694c21da6828db4c0c9e5702f695
Mark Gutenberger <gutenfries@gmail.com>
2022-11-03 21:59:20 -0500

    update sitemap.xml

--- routes/sitemap.xml.ts
+++ routes/sitemap.xml.ts
@@ -1,12 +1,24 @@
 import { Handlers } from '$fresh/server.ts';
-
 import { SitemapContext } from 'fresh_seo';
-
 import manifest from '@/fresh.gen.ts';
+import { listPosts } from '@/utils/blogPosts.ts';
 
 export const handler: Handlers = {
-	GET() {
-		const sitemap = new SitemapContext('https://gutenfries.deno.dev', manifest);
+	async GET() {
+		const posts = await listPosts();
+
+		const sitemap = new SitemapContext(
+			'https://gutenfries.deno.dev',
+			manifest,
+		);
+
+		for (const post of posts) {
+			sitemap.add(post.slug);
+		}
+
+		sitemap.add('/LICENSE.md');
+		sitemap.add('/robots.txt');
+
 		return sitemap.render();
 	},
 };
