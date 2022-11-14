7c6f539f531cc9e778ab213b24550e1d6225e6ff
Mark Gutenberger <gutenfries@gmail.com>
2022-11-14 07:54:02 -0600

    style the content picker better

--- components/ContentPicker.tsx
+++ components/ContentPicker.tsx
@@ -11,9 +11,14 @@ const ContentPicker = ({ hide }: { hide: boolean }) => {
 	}
 	return (
 		<>
-			<div className='z-30 fixed h-auto w-full bg-gray-300 bottom-0 border-t-2 border-blue-500'>
+			<div className='z-30 fixed h-auto w-full bg-gray-100 dark:bg-gray-900 bottom-0 border-t-2 border-blue-500'>
 				{/* close button */}
-				<div className='flex justify-end mt-4 mr-4'>
+				<div className='flex justify-end mt-4 mr-4 flex-grow'>
+					<div className='flex flex-row w-full -mr-12 flex justify-center items-center'>
+						<h2 className='my-2 text-3xl'>
+							What kind of content are you looking for?
+						</h2>
+					</div>
 					<a
 						href='/?content=undefined'
 						className='text-black'
@@ -23,27 +28,22 @@ const ContentPicker = ({ hide }: { hide: boolean }) => {
 					</a>
 				</div>
 				<div className='flex flex-col flex justify-center items-center'>
-					<h2 className='my-2 text-3xl'>
-						What kind of content are you looking for?
-					</h2>
-					<div className='flex flex-row flex justify-center items-center'>
-						<div className='flex flex-col flex justify-center items-center'>
-							<Link
-								className='text-lg mb-2'
-								href='/?content=tech'
-								u
-							>
-								Tech & Software Development
-							</Link>
+					<div className='flex flex-col flex justify-center items-center'>
+						<Link
+							className='text-lg mb-2'
+							href='/?content=tech'
+							u
+						>
+							Tech & Software Development
+						</Link>
 
-							<Link
-								className='text-lg mb-2'
-								href='/?content=music'
-								u
-							>
-								Music & Marching Arts
-							</Link>
-						</div>
+						<Link
+							className='text-lg mb-2'
+							href='/?content=music'
+							u
+						>
+							Music & Marching Arts
+						</Link>
 					</div>
 				</div>
 			</div>
