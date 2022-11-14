7b51d1633e93da09b0a91f38b5b0341aef4230b4
Mark Gutenberger <gutenfries@gmail.com>
2022-11-14 12:54:22 -0600

    add Sun and Moon Icons

--- utils/Icons.tsx
+++ utils/Icons.tsx
@@ -1432,6 +1432,55 @@ class Icons {
 			</svg>
 		);
 	};
+
+	public static Sun = (_?: IconProps): JSX.Element => {
+		return (
+			<svg
+				className={_?.className ?? undefined}
+				stroke={_?.stroke ?? 'currentColor'}
+				fill={_?.fill ?? 'none'}
+				xmlns='http://www.w3.org/2000/svg'
+				width={24}
+				height={24}
+				viewBox='0 0 24 24'
+				stroke-width='2'
+				stroke-linecap='round'
+				stroke-linejoin='round'
+				alt='Sun'
+			>
+				<title>Sun</title>
+				<desc>source: [Tabler Icons](https://tabler-icons.io)</desc>
+				<path stroke='none' d='M0 0h24v24H0z' fill='none'></path>
+				<circle cx='12' cy='12' r='4'></circle>
+				<path d='M3 12h1m8 -9v1m8 8h1m-9 8v1m-6.4 -15.4l.7 .7m12.1 -.7l-.7 .7m0 11.4l.7 .7m-12.1 -.7l-.7 .7'>
+				</path>
+			</svg>
+		);
+	};
+
+	public static Moon = (_?: IconProps): JSX.Element => {
+		return (
+			<svg
+				className={_?.className ?? undefined}
+				stroke={_?.stroke ?? 'currentColor'}
+				fill={_?.fill ?? 'none'}
+				xmlns='http://www.w3.org/2000/svg'
+				width={24}
+				height={24}
+				viewBox='0 0 24 24'
+				stroke-width='2'
+				stroke-linecap='round'
+				stroke-linejoin='round'
+				alt='Moon'
+			>
+				<title>Moon</title>
+				<desc>source: [Tabler Icons](https://tabler-icons.io)</desc>
+				<path stroke='none' d='M0 0h24v24H0z' fill='none'></path>
+				<path d='M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z'>
+				</path>
+			</svg>
+		);
+	};
 }
 
 export default Icons;
