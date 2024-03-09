3c8fc34e34fa63036fafa2bfa1d987af6ae74468
Marc Gutenberger <gutenfries@gmail.com>
2024-03-09 14:24:42 -0600

    config Deno/Fresh

--- main.ts
+++ main.ts
@@ -1,8 +1,11 @@
-/// <reference no-default-lib="true" />man
+/// <reference no-default-lib="true" />
 /// <reference lib="dom" />
 /// <reference lib="dom.iterable" />
 /// <reference lib="dom.asynciterable" />
 /// <reference lib="deno.ns" />
+
+import 'npm:@types/node';
+import '$std/dotenv/load.ts';
 
 import { start } from '$fresh/server.ts';
 import manifest from '@/fresh.gen.ts';
